68c91e8b514bfea42dd5c26acf923f2ff939b093
Kenyansa <kenyansajustine@gmail.com>
2023-07-17 09:56:45 +0300

    query selectors for necessary elements

---
+++ script.js
@@ -0,0 +1,14 @@
+let currentMusic = 0;
+
+const music = document.querySelector("#audio");
+
+const slider = document.querySelector(".slider");
+const songName = document.querySelector(".song-name");
+const artistName = document.querySelector(".artist-name");
+const disk = document.querySelector(".disk");
+const currentTime = document.querySelector(".current-time");
+const songDuration = document.querySelector(".song-duration");
+const playBtn = document.querySelector(".play-btn");
+const forwardBtn = document.querySelector(".forward-btn");
+const backwarBtn = document.querySelector("backward-btn");
+

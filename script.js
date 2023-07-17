b17d8b3c83d4cdf0ee43bcf8b07d1915e35485a4
Kenyansa <kenyansajustine@gmail.com>
2023-07-17 13:26:56 +0300

    assign JSON data to global varibale to be acessed anywhere

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentMusic = 0;
+let songData = []; //this global variable stores song data
 
 const music = document.querySelector("#audio");
-
 const slider = document.querySelector(".slider");
 const songName = document.querySelector(".song-name");
 const artistName = document.querySelector(".artist-name");
@@ -25,34 +25,36 @@ playBtn.addEventListener('click', ()=>{
 // setting up music to be played:
 const setMusic = (i)=>{
     slider.value = 0; //range value set to 0
-    // Fetch the JSON file and load the song data
-    fetch('songList.json')
-        .then(response => response.json())
-        .then(data=>{
-            //checking if JSON is being loaded
-            console.log(data);
-            //using the data
-            let song = data[i];
-            currentMusic = i;
-            music.src = song.path;
+    //using data from the JSON file
+    const song = songData[i];
+    currentMusic = i;
+    music.src = song.path;
 
-            songName.innerHTML = song.name;
-            artistName.innerHTML = song.artist;
-            disk.style.backgroundImage = `url('${song.cover}')`;
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    disk.style.backgroundImage = `url('${song.cover}')`;
 
-            currentTime.innerHTML = '00:00';
-            //without the setTimeOut function, there will be a slight delay when setting up the music sorce and accessing its duration
-            setTimeout(() => {
-                slider.max = music.duration;
-                songDuration.innerHTML = formatTime(music.duration);
-            }, 300);
-        })
-        //error handling
-        .catch(error => {
-            console.error('Error loading JSON file', error);
-        })
+    currentTime.innerHTML = '00:00';
+    //without the setTimeOut function, there will be a slight delay when setting up the music sorce and accessing its duration
+    setTimeout(() => {
+        slider.max = music.duration;
+        songDuration.innerHTML = formatTime(music.duration);
+    }, 300);
 }
-setMusic(9);
+// Fetch the JSON file and load the song data
+fetch('songList.json')
+    .then(response => response.json())
+    .then(data=>{
+    //checking if JSON is being loaded
+    console.log(data);
+    songData = data; //assign the data to the global variable
+    setMusic(9); //call setMusic after the data is loaded
+    })
+    //error handling
+    .catch(error => {
+        console.error('Error loading JSON file', error);
+    });
+
 //formatting the song duration to mins and seconds format
 const formatTime = (time)=>{
     let min = Math.floor(time / 60);
@@ -77,10 +79,21 @@ slider.addEventListener('change', ()=>{
 
 //forward and backward button functions
 forwardBtn.addEventListener('click', ()=>{
-    if(currentMusic <= data.length - 1){
+    if(currentMusic >= songData.length - 1){
         currentMusic = 0;
     }else{
         currentMusic++;
     }
     setMusic(currentMusic);
+    playBtn.click();
+});
+
+backwardBtn.addEventListener('click', ()=>{
+    if(currentMusic <= 0){
+        currentMusic = songData.length - 1;
+    }else{
+        currentMusic--;
+    }
+    setMusic(currentMusic);
+    playBtn.click();
 })
